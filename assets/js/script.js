11015203e22cc4f84322a0b2fa19e910c3d898ad
charles johnston <charlesjohnston2002@yahoo.com>
2022-01-14 15:56:05 -0500

    basic intro page designed

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var seconds = 75;
+
+// Add event listener to 'start quiz' button, should start timer and launch first question
+timerStart.addEventListener("click", quizBegins);
+
+
+// will need an event listener for each submission for questions 1 - 10
+
+
+var quizBegins = function(event) {
+    // get target element from event
+    var targetEl = event.target;
+    // start quiz with question 1
+};
+
+
+
