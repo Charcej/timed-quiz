7a680bd4380f06ebcc9006feacd096c0f0ebc129
charles johnston <charlesjohnston2002@yahoo.com>
2022-01-15 15:27:04 -0500

    added some stuff and second js file

---
+++ assets/js/questions.js
@@ -0,0 +1,15 @@
+// array of numbers, questions, options and correct answer to be called upon
+var questions = [
+    {
+       number: 1,
+       question: "Commonly used data types do not include:",
+       answer: "3. alerts",
+       options: [
+           "1. strings",
+           "2. booleans",
+           "3. alerts",
+           "4. numbers"
+       ]
+    },
+
+] 
